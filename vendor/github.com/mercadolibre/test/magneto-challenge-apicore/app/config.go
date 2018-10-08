5017e98735bb2b6a76e8e2bb151d3c577e7daccb
Florencia Solari <florencia.solari@mercadolibre.com>
2018-10-08 13:35:49 -0300

    vendor

--- vendor/github.com/mercadolibre/test/magneto-challenge-apicore/app/config.go
+++
@@ -1,6 +0,0 @@
-package app
-
-const (
-	LogFilePath = "./logs.log"
-)
-
--- vendor/github.com/mercadolibre/test/magneto-challenge-apicore/domain/Stats.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-type Stats struct {
-	CountMutantDna int `json:"count_mutant_dna"`
-	CountHumanDna  int `json:"count_human_dna"`
-	Ratio          float64 `json:"ratio"`
-}
