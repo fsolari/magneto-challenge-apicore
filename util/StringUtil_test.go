1fe3a99bec93f470d6cc205fe831ac558d174957
Florencia Solari <florencia.solari@mercadolibre.com>
2018-10-09 15:12:44 -0300

    util test

--- util/StringUtil_test.go
+++ util/StringUtil_test.go
@@ -19,7 +19,7 @@ func TestStringContainsIfNotPresentMustReturnFalse(t *testing.T) {
 	var a = []string{"H", "E", "L", "L", "O"}
 	var c = "W"
 
-	ok := util.StringArrayContains(a, c)
+	ok := StringArrayContains(a, c)
 
 	assert.False(t, ok, fmt.Sprintf("ok must be false %v", ok))
 }
